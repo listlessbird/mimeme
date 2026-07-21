4b443d8f14df732f4e10d9c2c7023e9be14edb85
listlessbird <124798751+listlessbird@users.noreply.github.com>
2026-07-21 14:24:50 +0530

    why not

--- webui/src/views/home.tsx
+++ webui/src/views/home.tsx
@@ -12,11 +12,11 @@ export default function Home() {
 				className="aspect-square w-full max-w-85 text-foreground sm:aspect-auto sm:h-[clamp(400px,68vh,760px)] sm:max-w-4xl"
 				gridSize={280}
 				scale={isMobile ? 0.92 : 1.05}
-				dotScale={1}
+				dotScale={0.8}
 				invert={true}
 				cornerRadius={0.2}
 				gamma={1.0}
-				blur={3}
+				blur={3.75}
 				diffusionStrength={1.0}
 			/>
 			<div className="w-full max-w-xl">
